f2d8e85aa9dbc5b48079f922e274ee6dcd94a91b
Mārtiņš Tālbergs <martins.talbergs@zabbix.com>
2021-10-26 20:38:54 +0300

    Release 0.19.0-rc2

--- grammars/php/grammar.php
+++ grammars/php/grammar.php
@@ -12,11 +12,26 @@ function php(): CData
     static $ffi;
 
     if (!$ffi) {
+        // https://clang.llvm.org/docs/CrossCompilation.html
+        $arch = php_uname('m');
+        $vendor = match (PHP_OS_FAMILY) {
+            'Linux', 'BSD' => 'linux',
+            'Darwin' => 'apple',
+            'Windows' => 'w64',
+        };
+        $abi = match (PHP_OS_FAMILY) {
+            'Linux', 'BSD' => 'gnu',
+            'Darwin' => 'darwin',
+            'Windows' => 'mingw32',
+        };
+
+        $bin = sprintf('%s-%s-%s.%s', $arch, $vendor, $abi, PHP_SHLIB_SUFFIX);
+
         $ffi = FFI::cdef(
             'typedef struct TSLanguage TSLanguage;'
             . PHP_EOL
             . 'TSLanguage *tree_sitter_php();',
-            __DIR__ . '/' . php_uname('m') . '.so',
+            __DIR__ . '/' . $bin,
         );
     }
 
